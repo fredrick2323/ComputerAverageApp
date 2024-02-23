d756263f5124d61a5feee130203e9feea3c2fe6d
fredrick2323 <fredrickdorong3@gmail.com>
2024-02-23 10:14:58 +0800

    Add project files.

---
+++ ComputerAverageApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ComputerAverageApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter 5 grade separated by new Line; ");
+        double grade1 = Convert.ToDouble(Console.ReadLine());
+        double grade2 = Convert.ToDouble(Console.ReadLine());
+        double grade3 = Convert.ToDouble(Console.ReadLine());
+        double grade4 = Convert.ToDouble(Console.ReadLine());
+        double grade5 = Convert.ToDouble(Console.ReadLine());
+
+
+        double sum = grade1 + grade2 + grade3 + grade4 + grade5;
+        double totalgrades = sum / 5;
+        double average = Math.Round(totalgrades);
+        Console.WriteLine("The average is " + totalgrades + " and round off to " + average);
+    }
+
+}
